eac93fc509cf8b56af5e5ad43c2570dcd61a97ff
Hannah Robinson <hannah.robinson@vuw.ac.nz>
2022-10-17 15:08:14 +1300

    added new functionality

--- mars.sh
+++ mars.sh
@@ -2,3 +2,4 @@
 echo "Cold and dry, but everything is my favourite colour"
 echo "The two moons may be a problem for Wolfman"
 echo "But the mummy will appreciate the lack of humidity"
+error is here
