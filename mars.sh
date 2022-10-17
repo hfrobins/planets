f4498a6f136e51674199bb5b544227aff3b362b4
Hannah Robinson <hannah.robinson@vuw.ac.nz>
2022-10-17 15:12:01 +1300

    Revert "added new functionality"
    This reverts commit eac93fc509cf8b56af5e5ad43c2570dcd61a97ff.

--- mars.sh
+++ mars.sh
@@ -2,4 +2,3 @@
 echo "Cold and dry, but everything is my favourite colour"
 echo "The two moons may be a problem for Wolfman"
 echo "But the mummy will appreciate the lack of humidity"
-error is here
