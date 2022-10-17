58f10db4d28ebbf00b0c809660e95e36edf40e74
Hannah Robinson <hannah.robinson@vuw.ac.nz>
2022-10-17 16:17:56 +1300

    Don't know

--- mars.sh
+++ mars.sh
@@ -2,3 +2,4 @@
 echo "Cold and dry, but everything is my favourite colour"
 echo "The two moons may be a problem for Wolfman"
 echo "But the mummy will appreciate the lack of humidity"
+echo "Making a change"
